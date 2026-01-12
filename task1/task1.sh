7df82ac13a31a15eee22c7b3cc5ebc4484307874
sumail829 <samirpanjiyar4@gmail.com>
2026-01-12 08:24:29 +0000

    updated permisiion

--- task1/task1.sh
+++ task1/task1.sh
@@ -3,4 +3,4 @@
 
 #!/usr/bin/env bash
 
-sudo du /var/lib/jenkins -h --max-depth=1  | sort -hr | head -n 10 > /tmp/large_file.txt
+du /var/lib/jenkins -h --max-depth=1  | sort -hr | head -n 10 > /tmp/large_file.txt
