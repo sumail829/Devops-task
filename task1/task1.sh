d6576a2da82e3a610e348c4dd586dd7eac136b67
sumail829 <samirpanjiyar4@gmail.com>
2026-01-12 09:53:27 +0000

    updated shebang

--- task1/task1.sh
+++ task1/task1.sh
@@ -1,6 +1,3 @@
-# A production server is runnug out of disk space.
-# Find the top 10 largest files on the system and save the result to /tmp/large_files.txt
-
-#!/usr/bin/env bash
+#!/bin/bash
 
 du /var/lib/jenkins -h --max-depth=1  | sort -hr | head -n 10 > /tmp/large_file.txt
