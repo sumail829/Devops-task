65d09bb3f31e0b0aa9f98d086d20fd1daa6e4640
sumail829 <samirpanjiyar4@gmail.com>
2026-01-13 06:53:03 +0000

    added task2.sh

--- task2/task2.sh
+++ task2/task2.sh
@@ -1,6 +1,6 @@
 #find the logs greater than 14 days in /var/log and delete them without deleting the recent log
 
-#!/usr/bin/env bash 
+#!/bin/bash
 
 find /var/log -type f -mtime +14 -print
 find /var/log -type f -mtime +14 -delete
